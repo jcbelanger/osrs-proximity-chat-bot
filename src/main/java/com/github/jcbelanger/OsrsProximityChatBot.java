3dc0f4b443c915c113a7b3993bb7445978864e38
Joshua Belanger <jbelanger@penbaytechgroup.com>
2025-01-04 17:32:37 -0500

    init

---
+++ src/main/java/com/github/jcbelanger/OsrsProximityChatBot.java
@@ -0,0 +1,13 @@
+package com.github.jcbelanger;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OsrsProximityChatBot {
+
+    public static void main(String[] args) {
+        log.info("info asdf");
+        log.error("itest");
+    }
+}
