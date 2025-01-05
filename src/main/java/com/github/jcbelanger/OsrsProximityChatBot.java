6feb91f53e587ccdd9aa43dcb8dfb09a4b81bd5a
Joshua Belanger <jbelanger@penbaytechgroup.com>
2025-01-05 00:42:42 -0500

    discord bot + spring boot

--- src/main/java/com/github/jcbelanger/OsrsProximityChatBot.java
+++ src/main/java/com/github/jcbelanger/OsrsProximityChatBot.java
@@ -1,13 +1,12 @@
 package com.github.jcbelanger;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
+@SpringBootApplication
 public class OsrsProximityChatBot {
 
     public static void main(String[] args) {
-        log.info("info asdf");
-        log.error("itest");
+        SpringApplication.run(OsrsProximityChatBot.class, args);
     }
 }
